5b5200e9c6e9101116063f96d899aa40448d3d68
Ubuntu <ubuntu@ip-172-31-10-142.ap-south-1.compute.internal>
2025-05-11 13:54:27 +0000

    Add new feature route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -9,4 +10,23 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+// Existing route
+app.get('/', (req, res) => {
+  res.send('Fantasy Cricket App Running');
+});
+
+// New feature route
+app.get('/api/v1/new-feature', (req, res) => {
+  res.json({ message: "New feature successfully added!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 
